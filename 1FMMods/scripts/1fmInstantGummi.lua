5f00d60a381546c438a36bb57943fba2152a32c8
Denhonator <elmeri.uotila@gmail.com>
2021-04-09 18:32:26 +0300

    Fixed going to Deep Jungle and Neverland
    Still buggy when re-entering

--- 1FMMods/scripts/1fmInstantGummi.lua
+++ 1FMMods/scripts/1fmInstantGummi.lua
@@ -1,3 +1,7 @@
+local warpTo0 = 0
+local prevWarp = 0
+local warpAdd = 0
+
 function _OnInit()
 
 end
@@ -10,13 +14,26 @@ function _OnFrame()
 	
 	local monstroOpen = ReadByte(0x2DE78CA-0x3A0606) > 1
 	local neverlandOpen = ReadByte(0x2DE78C7-0x3A0606) > 2
+	local deepJungleOpen = ReadByte(0x2DE78C3-0x3A0606) > 2
 
+	if not deepJungleOpen and realWorld == 5 then
+		warpTo0 = realWorld
+	end
+	if not neverlandOpen and (realWorld == 13 or realWorld == 7) then
+		warpTo0 = realWorld
+	end
+	if warpTo0 > 0 and (room ~= 0 or selection ~= warpTo0) then
+		warpTo0 = 0
+		WriteByte(warpAdd-0x3A0606, prevWarp) --room mod
+	end
+	if warpTo0 > 0 then
+		warpAdd = realWorld==5 and 0x6A8640 or 0x6A8640-0x400
+		WriteByte(warpAdd-0x3A0606, realWorld==5 and 0 or 6) --room mod
+	end
+	
 	if not monstroOpen and (selection == 10 or selection == 9) then
 		selection = selection == 9 and 18 or 17
 		--WriteInt(0x503CEC-0x3A0606, selection)
-	end
-	if not neverlandOpen and selection == 13 then
-		selection = 19
 	end
 	if selection == 25 then
 		selection = 15
