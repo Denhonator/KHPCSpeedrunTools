943513b906ea6ce5451de2deb9c889e646773303
Denhonator <elmeri.uotila@gmail.com>
2021-04-11 11:59:52 +0300

    Update 1fmFasterDialog.lua

--- 1FMMods/scripts/1fmFasterDialog.lua
+++ 1FMMods/scripts/1fmFasterDialog.lua
@@ -1,4 +1,5 @@
 local lastProg = 0
+local textSpeedup = false
 
 function _OnInit()
 
@@ -9,11 +10,11 @@ function _OnFrame()
 	
 	WriteFloat(0x22E8744-0x3A0606, 0) --finishes box transitions
 
-	--if textProg > lastProg and lastProg > 0 then --1 frame turbo
-	--	WriteFloat(0x233C25C-0x3A0606, 10.0)
-	--elseif ReadFloat(0x233C25C-0x3A0606) > 1.0 then
-	--	WriteFloat(0x233c25c-0x3A0606, 1.0)
-	--end
+	if textProg > lastProg and lastProg > 0 and textSpeedup then --1 frame turbo
+		WriteFloat(0x233C25C-0x3A0606, 100.0)
+	elseif ReadFloat(0x233C25C-0x3A0606) > 1.0 then
+		WriteFloat(0x233c25c-0x3A0606, 1.0)
+	end
 
 	lastProg = textProg
 end
