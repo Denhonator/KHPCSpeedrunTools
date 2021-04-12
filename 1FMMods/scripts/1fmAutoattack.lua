839084ac19b658ba23954b16dff212b075afcce4
Denhonator <elmeri.uotila@gmail.com>
2021-04-12 16:13:25 +0300

    Added cooldown to autoattack after dialog

--- 1FMMods/scripts/1fmAutoattack.lua
+++ 1FMMods/scripts/1fmAutoattack.lua
@@ -1,3 +1,5 @@
+local cooldown = 0
+
 function _OnInit()
 
 end
@@ -5,8 +7,14 @@ end
 function _OnFrame()
 	local attackInput = (ReadByte(0x233D035-0x3A0606)//2//2//2//2//2//2)%2 == 1
 	local menu = ReadInt(0x232A600-0x3A0606) == 1
+	local dialog = ReadInt(0x2863958-0x3A0606) > 0
+	if menu or dialog then
+		cooldown = 20
+	elseif cooldown > 0 then
+		cooldown = cooldown - 1
+	end
 	local attackCommand = ReadByte(0x52558C-0x3A0606) == 0
-	local autofireState = (attackInput and attackCommand and not menu)  and 1 or 0
+	local autofireState = (attackInput and attackCommand and cooldown == 0)  and 1 or 0
 	WriteInt(0x23D0600-0x3A0606, autofireState)
 	WriteInt(0x232A444-0x3A0606, autofireState)
 end
