894102e5ada300cfe29aec87660fab49875f569b
Denhonator <elmeri.uotila@gmail.com>
2021-04-18 14:27:58 +0300

    Fixed Monstro/DI softlock/crash

--- 1FMMods/scripts/1fmRandoNoLogic.lua
+++ 1FMMods/scripts/1fmRandoNoLogic.lua
@@ -10,11 +10,15 @@ local goofyAbilityTable = donaldAbilityTable+0x198
 local rewardTable = btltbl+0xC6A8
 local chestTable = 0x5259E0
 
-local TTWarp = 0x5229B0+0x9B570C+6
+--local TTWarp = 0x5229B0+0x9B570C+6
 local worldWarps = 0x50B940
 local worldFlagBase = 0x2DE79D0+0x6C
 local gummiFlagBase = 0x2DE78C0
 local worldMapLines = 0x2DE78E2
+local gummiselect = 0x503CEC
+local unlockedWarps = 0x2DE78D6
+local warpCount = 0x50BA30
+local monstroCutsceneFlag = 0x2DE65D0-0x200+0xB09
 
 local enableRC = 0x2DE6244
 local lockMenu = 0x232A60C
@@ -38,12 +42,20 @@ function InstantGummi()
 end
 
 function _OnFrame()
-	local TTWorld = ReadByte(TTWarp-offset)
-	if TTWorld==3 then
-		WriteByte(TTWarp-offset, 1) -- Change it to DI world ID
-		WriteShort(worldWarps+0xE8-offset, 1) -- Add DI warp
-		WriteShort(worldWarps+0xF0-offset, 0xFFFF)
-		WriteByte(worldFlagBase+7-offset, 0)
+	if ReadByte(unlockedWarps-7-offset) < 8 then
+		WriteByte(unlockedWarps-7-offset, 9)
+		WriteByte(warpCount+4*3-offset, 4)
+	end
+	if ReadByte(gummiselect-offset)==3 then
+		--WriteByte(TTWarp-offset, 1) -- Change it to DI world ID
+		WriteShort(worldWarps+0x18-offset, 1) -- Add DI warp
+		--WriteByte(worldFlagBase+7-offset, 0) -- Fix up story flags
+	else
+		WriteShort(worldWarps+0x18-offset, 4) -- Revert to Wonderland
+	end
+	
+	if ReadByte(monstroCutsceneFlag-offset) < 0x14 then
+		WriteByte(monstroCutsceneFlag-offset, 0x14)
 	end
 	
 	if ReadInt(input-offset) == 3848 then
@@ -69,5 +81,15 @@ function _OnFrame()
 	
 	if ReadByte(enableRC-offset) ~= 0x0 then
 		WriteByte(enableRC-offset, 0x0)
+	end
+	
+	if ReadByte(0x232A604-offset) then
+		WriteByte(0x2E1CC28-offset, 3) --Unlock gummi
+		WriteByte(0x2E1CB9C-offset, 5) --Set 5 buttons to save menu
+		WriteByte(0x2E8F450-offset, 5) --Set 5 buttons to save menu
+		WriteByte(0x2E8F452-offset, 5) --Set 5 buttons to save menu
+		for i=0,4 do
+			WriteByte(0x2E1CBA0+i*4-offset, i) --Set button types
+		end
 	end
 end
