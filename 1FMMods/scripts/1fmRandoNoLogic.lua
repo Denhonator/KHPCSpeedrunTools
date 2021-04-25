21272550fbec2a40849f678bc4c1d02ee26f5056
Denhonator <elmeri.uotila@gmail.com>
2021-04-25 10:37:10 +0300

    Black screen removal only in EotW

--- 1FMMods/scripts/1fmRandoNoLogic.lua
+++ 1FMMods/scripts/1fmRandoNoLogic.lua
@@ -845,7 +845,7 @@ function _OnFrame()
 		removeBlackTimer = 0
 	end
 	
-	if HUDNow > 0 or removeBlackTimer > 600 then
+	if HUDNow > 0 or (removeBlackTimer > 300 and ReadByte(world)==0x10) then
 		WriteInt(0x233C450-offset, 128) --Remove black screen
 		WriteInt(0x233C454-offset, 128)
 		WriteInt(0x233C458-offset, 128)
