6be88b44a93c729d812103532d884be6fc2a35ae
Denhonator <elmeri.uotila@gmail.com>
2021-05-22 00:19:59 +0300

    Party now back in DI (but don't menu their stuff)

--- 1FMMods/scripts/1fmRandoSomeLogic.lua
+++ 1FMMods/scripts/1fmRandoSomeLogic.lua
@@ -1818,12 +1818,6 @@ function InstantGummi()
 end
 
 function FlagFixes()
-	-- Remove party in DI
-	if ReadByte(world) == 1 and ReadFloat(soraHUD) > 0 and ReadByte(inGummi) == 0 then
-		WriteByte(party1, 0xFF)
-		WriteByte(party1+1, 0xFF)
-	end
-
 	-- Reset TT to avoid softlocks
 	if ReadByte(cutsceneFlags+0xB04) < 0x14 and ReadByte(world) ~= 3 then
 		WriteByte(cutsceneFlags+0xB04, 0)
