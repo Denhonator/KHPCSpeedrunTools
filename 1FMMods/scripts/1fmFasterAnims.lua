f2e01b70b4cb8e95ab02016ac989fdc2a43eb813
Denhonator <elmeri.uotila@gmail.com>
2021-06-02 23:30:48 +0300

    Bug fix

--- 1FMMods/scripts/1fmFasterAnims.lua
+++ 1FMMods/scripts/1fmFasterAnims.lua
@@ -1,6 +1,5 @@
 local summonSpeedup = true
 local speedMult = 2.0
-local lastProg = 0
 local offset = 0x3A0606
 local soraHUD = 0x280EB1C - offset
 
@@ -24,14 +23,12 @@ function _OnFrame()
 	local skippable = ReadInt(0x23944E4-offset)
 	local summoning = ReadInt(0x2D5D62C-offset)
 
-	if ReadFloat(soraHUD) == 0 and cutscene > 0 and cutscene ~= 8 
+	if ReadFloat(soraHUD) < 1 and cutscene > 0 and cutscene ~= 8 
 					and skippable ~= 1025 and (summoning == 0 or summonSpeedup) then
 		WriteFloat(0x233C24C-offset, speedMult)
 	else
 		WriteFloat(0x233C24C-offset, 1.0)
 	end
 
-	lastProg = textProg
-	
 	::done::
 end
