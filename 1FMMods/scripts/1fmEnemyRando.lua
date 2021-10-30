1f5dbd031c365dc122a4c5fd6972de25e62f83c5
Denhonator <elmeri.uotila@gmail.com>
2021-10-30 20:25:38 +0300

    Fixes

--- 1FMMods/scripts/1fmEnemyRando.lua
+++ 1FMMods/scripts/1fmEnemyRando.lua
@@ -23,8 +23,15 @@ function _OnInit()
 	if GAME_ID == 0xAF71841E and ENGINE_TYPE == "BACKEND" then
 		ConsolePrint("KH1 detected, running script")
 		canExecute = true
-		repl["xa_ex_2060"] = "xa_ex_1040"
-		repl["xa_ex_2020"] = "xa_ex_2230"
+		--repl["xa_ex_2060"] = "xa_ex_1040"
+		--repl["xa_ex_2010"] = "xa_ex_2230"
+		--repl["xa_ex_2130"] = "xa_pi_3000"
+		--repl["xa_pp_3030"] = "xa_pi_3000"
+		--repl["xa_tw_3000"] = "xa_pi_3000"
+		repl["xa_ex_1030"] = "xa_ex_1040"
+		repl["xa_ex_1630"] = "xa_pp_3000"
+		repl["xa_di_3000"] = "xa_pi_3000"
+		
 	else
 		ConsolePrint("KH1 not detected, not running script")
 	end
@@ -33,47 +40,46 @@ end
 function WriteString(addr, s)
 	for i=0,#s-1 do
 		WriteByte(addr+i, string.byte(s, i+1), false)
-		WriteByte(addr+i+0x20, string.byte(s, i+1), false)
 	end
-	ConsolePrint("Replaced")
+	ConsolePrint(string.format("Replaced: %s", s))
 end
 
 function _OnFrame()
 	local input = ReadInt(0x233D034-offset)
-	if lastAddr > 0 and lastAddr < 0x86F9FA and replaced then
-		WriteByte(lastAddr, 0)
-		lastAddr = 0
-		replaced = false
-	end
-
+	
 	if canExecute and ReadInt(blackfade) == 0 and not hasChanged and input~=8 then
 		local addr = 0x46F9FA
-		while addr < 0x86F9FA and ReadShort(addr) ~= 0x6178 do
-			addr = addr + 0x80
-		end
-		lastAddr = addr
-		local loopcount = 0
-		while loopcount < 60 and addr < 0x86F9FA do
-			local r = {}
-			local empty = ReadByte(addr, false) < 97 or ReadByte(addr, false) > 122
-			for i=0,9 do
-				r[i+1] = string.char(ReadByte(addr+i, false))
+		while addr < 0x86F9FA do
+			while addr < 0x86F9FA and ReadShort(addr) ~= 0x6178 or ReadShort(addr-0x20) ~= 0 do
+				addr = addr + 0x80
 			end
-			local s = table.concat(r)
-			if not empty then
-				if repl[s] ~= nil then
-					WriteString(addr, repl[s])
-					monitor = addr
-					lastMonitor = ReadLong(monitor)
-					replaced = true
+			if addr < 0x86F9FA then
+				ConsolePrint(string.format("%x", addr+offset))
+			end
+			lastAddr = addr
+			local loopcount = 0
+			while loopcount < 120 and addr < 0x86F9FA do
+				local r = {}
+				local empty = ReadByte(addr, false) < 97 or ReadByte(addr, false) > 122
+				for i=0,9 do
+					r[i+1] = string.char(ReadByte(addr+i, false))
 				end
-				hasChanged = true
-				ConsolePrint(s)
-			elseif loopcount > 0 then
-				break
+				local s = table.concat(r)
+				if not empty then
+					if repl[s] ~= nil and ReadByte(addr+10) == 0x2E then
+						WriteString(addr, repl[s])
+						monitor = addr+2
+						lastMonitor = ReadLong(monitor)
+						replaced = true
+					end
+					hasChanged = true
+					ConsolePrint(s)
+				elseif loopcount > 0 then
+					break
+				end
+				loopcount = loopcount + 1
+				addr = addr + 0x20
 			end
-			loopcount = loopcount + 1
-			addr = addr + 0x40
 		end
 	end
 	if ReadByte(blackfade) == 128 or ReadLong(monitor) ~= lastMonitor then
