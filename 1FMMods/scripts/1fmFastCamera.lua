5a95ea2079c751783750d24da91bfd81c39429e8
Denhonator <elmeri.uotila@gmail.com>
2022-04-07 12:21:44 +0300

    Many improvements to FastCamera

--- 1FMMods/scripts/1fmFastCamera.lua
+++ 1FMMods/scripts/1fmFastCamera.lua
@@ -2,11 +2,17 @@ LUAGUI_NAME = "1fmFastCamera"
 LUAGUI_AUTH = "denhonator"
 LUAGUI_DESC = "Speeds up camera movement, instant R3"
 
+local overallSpeed = 1.4
+local accelerationSpeed = 0.01
+
 local offset = 0x3A0606
 -- For some reason can't write to these addresses
 local accel = 0x3E7F58 - offset
 local deaccel = 0x3E7F5C - offset
-local curSpeed = 0x25344C4 - offset
+local curSpeedV = 0x25344C0 - offset
+local curSpeedH = 0x25344C4 - offset
+local cameraInputH = 0x233D060 - offset
+local cameraInputV = 0x233D064 - offset
 -----
 local snap = 0x1DD299 - offset
 local accelHack = 0x1E2924 - offset
@@ -15,7 +21,8 @@ local speed = 0x503A1C - offset
 
 local versionCheck = 0x3D6AB8 - offset
 
-local lastSpeed = 0
+local lastSpeedH = 0
+local lastSpeedV = 0
 
 local canExecute = false
 
@@ -39,14 +46,27 @@ function _OnInit()
 end
 
 function _OnFrame()
-	local currentSpeed = ReadFloat(curSpeed)
-	if canExecute and math.abs(ReadFloat(speed)) == 1.0 then -- This way it works for inverted camera
-		-- You can change the 1.4 to anything you wish. Bigger = faster.
-		WriteFloat(speed, ReadFloat(speed) * 1.4)
-	elseif canExecute and math.abs(currentSpeed) > math.abs(lastSpeed) then
-		WriteFloat(curSpeed, math.min(math.max(currentSpeed * 2, -0.44), 0.44))
-	elseif canExecute and math.abs(currentSpeed) < math.abs(lastSpeed) then
-		WriteFloat(curSpeed, currentSpeed * 0.5)
+	if canExecute then
+		local currentSpeedH = ReadFloat(curSpeedH)
+		local currentSpeedV = ReadFloat(curSpeedV)
+		local difH = currentSpeedH - lastSpeedH
+		local difV = currentSpeedV - lastSpeedV
+		
+		if math.abs(ReadFloat(speed)) == 1.0 then -- This way it works for inverted camera
+			WriteFloat(speed, ReadFloat(speed) * overallSpeed)
+		end
+		
+		if math.abs(ReadFloat(cameraInputH)) > 0.05 then
+			WriteFloat(curSpeedH, math.min(math.max(currentSpeedH + ReadFloat(cameraInputH) * accelerationSpeed, -0.44), 0.44))
+		else
+			WriteFloat(curSpeedH, currentSpeedH * (1.0 - accelerationSpeed * 10))
+		end
+		if math.abs(ReadFloat(cameraInputV)) > 0.05 then
+			WriteFloat(curSpeedV, math.min(math.max(currentSpeedV + ReadFloat(cameraInputV) * accelerationSpeed, -0.44), 0.44))
+		else
+			WriteFloat(curSpeedV, currentSpeedV * (1.0 - accelerationSpeed * 10))
+		end
+		lastSpeedH = currentSpeedH
+		lastSpeedV = currentSpeedV
 	end
-	lastSpeed = currentSpeed
 end
