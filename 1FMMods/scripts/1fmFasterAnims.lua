5b6c953e93f5b965f8b5e8f5c133719ce3aaa371
Denhonator <elmeri.uotila@gmail.com>
2022-06-12 16:40:17 +0300

    Fixed 100 acre timer issue and sped up Riku race (rip)

--- 1FMMods/scripts/1fmFasterAnims.lua
+++ 1FMMods/scripts/1fmFasterAnims.lua
@@ -26,9 +26,15 @@ function _OnFrame()
 	local cutscene = ReadInt(0x2378B60-offset)
 	local skippable = ReadInt(0x23944E4-offset)
 	local summoning = ReadInt(0x2D5D62C-offset)
+	local world = ReadByte(0x233CADC-offset)
+	local room = ReadByte(0x233CB44-offset)
+	local minitimer = ReadInt(0x232A684-offset)
+	local camstate = ReadByte(0x2998188-offset)
 
 	if ReadFloat(soraHUD) < 1 and cutscene > 0 and cutscene ~= 8 
-					and skippable ~= 1025 and (summoning == 0 or summonSpeedup) then
+		and skippable ~= 1025 and (summoning == 0 or summonSpeedup)
+		and not (world==6 and room==8 and minitimer>=18000.0/speedMult)
+		and not (world==1 and room==2 and (camstate>=1 and camstate<=5)) then
 		WriteFloat(0x233C24C-offset, speedMult)
 	else
 		WriteFloat(0x233C24C-offset, 1.0)
