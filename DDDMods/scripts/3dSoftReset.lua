2e979f11d512d42a12aa117f28f1f9c02744b202
Chris Anthony Hudson <c.ahudson84@yahoo.com>
2024-04-13 03:14:12 -0700

    3D Soft reset updates (#46)
    * Minor fixes for broken splits.
    
    * Confirmed Any % update
    
    * Fixed 3D soft reset.
    
    ---------
    
    Co-authored-by: Chris Anthony Hudson <chris.hudson@varsitytutors.com>

--- DDDMods/scripts/3dSoftReset.lua
+++ DDDMods/scripts/3dSoftReset.lua
@@ -1,15 +1,15 @@
 LUAGUI_NAME = "dddSoftReset"
-LUAGUI_AUTH = "denhonator"
+LUAGUI_AUTH = "deathofall84 (original by denhonator)"
 LUAGUI_DESC = "Soft reset with shoulder buttons + start"
 
-local offset = 0x770EFA
+local offset = 0x770E4A
 
 function _OnInit()
 end
 
 function _OnFrame()
-	local input = ReadInt(0x354E16-offset)
+	local input = ReadInt(0x9E6C08-offset)
 	if (input == 0x0F08) then 
-		WriteByte(0x354FBE-offset, 0x01)
+		WriteByte(0xAC5EB8-offset, 0x01)
 	end
 end
