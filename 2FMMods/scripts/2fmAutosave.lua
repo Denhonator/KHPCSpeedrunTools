a3e028a7f980efbd2ce86cc520f6317901f0c8db
codename-geek <155425660+codename-geek@users.noreply.github.com>
2024-07-03 05:47:11 -0700

    Update 2fmAutosave.lua (#68)
    - Updated to support the updated Steam and Epic versions

--- 2FMMods/scripts/2fmAutosave.lua
+++ 2FMMods/scripts/2fmAutosave.lua
@@ -1,10 +1,4 @@
-local offset = 0x56450E
-local offset2 = 0x56454E
-
-local continue = 0x29F8C80 - offset2
-local save1 = 0x9A70B0 - offset2
-local saveselect = 0x2614A20 - offset2
-
+local GameVersion = 0
 local canExecute = false
 local prevBlack = 0
 local prevContinue = 0
@@ -12,37 +6,70 @@ local blacklist = {"es01", "bb05", "eh20", "eh22", "eh23", "eh24", "eh25", "eh26
 local blacklisted = false
 
 function _OnInit()
-	if GAME_ID == 0x431219CC and ENGINE_TYPE == "BACKEND" then
-		canExecute = true
-		ConsolePrint("KH2 detected, running script")
-		if ReadInt(0x2A5A056-offset) > 0 and ReadInt(0x2A59056-offset) == 0 then
-			offset = 0x56550E
-			offset2 = 0x56554E
-			ConsolePrint("Detected JP version. If this is incorrect, try reloading at a different time")
-		else
-			ConsolePrint("Detected GLOBAL version. If this is incorrect, try reloading at a different time")
+	GameVersion = 0
+end
+
+function GetVersion() --Define anchor addresses
+	if GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then --PC
+		if ReadString(0x09A92F0,4) == 'KH2J' then --EGS
+			GameVersion = 2
+			print('[2fmAutoSave] - Epic Version Detected')
+			continue = 0x29FAFC0
+			save1 = 0x9A92F0
+			saveselect = 0x2616D60
+			sveAddr = 0x9BA370
+			inputAddr = 0x29FAD30
+			LoadingIndicator = 0x8EBFF0
+			loadmenu = 0x743350
+			writeLogic = 0x713438
+            canExecute = true
+		elseif ReadString(0x09A9830,4) == 'KH2J' then --Steam Global
+			GameVersion = 3
+			print('[2fmAutoSave] - Steam Global Version Detected')
+			continue = 0x29FB500
+			save1 = 0x9A9830
+			saveselect = 0x2617460
+			sveAddr = 0x9BA8B0
+			inputAddr = 0xBF3120
+			LoadingIndicator = 0x8EC540
+			loadmenu = 0x7435D0
+			writeLogic = 0x7134A0
+            canExecute = true
+		elseif ReadString(0x09A8830,4) == 'KH2J' then --Steam JP
+			GameVersion = 4
+			print('[2fmAutoSave] - Steam JP Version Detected')
+			continue = 0x29FA500
+			save1 = 0x9A8830
+			saveselect = 0x2616460
+			sveAddr = 0x9B98B0
+			inputAddr = 0xBF2120
+			LoadingIndicator = 0x8EB540
+			loadmenu = 0x7425D0
+			writeLogic = 0x7124A0
+            canExecute = true
 		end
-	else
-		ConsolePrint("KH2 not detected, not running script")
 	end
 end
 
 function _OnFrame()
-SVE = ReadString(0x9B8130 - offset2, 4)
-for i = 1, 11 do
-	if SVE ~= blacklist[i] then
-	blacklisted = false
+	if GameVersion == 0 then --Get anchor addresses
+		GetVersion()
+		return
 	end
-end
-for i = 1, 11 do
-	if SVE == blacklist[i] then
-	blacklisted = true
+	
+	SVE = ReadString(sveAddr, 4)
+	for i = 1, 11 do
+		if SVE ~= blacklist[i] then
+		blacklisted = false
+		end
 	end
-end
+	for i = 1, 11 do
+		if SVE == blacklist[i] then
+		blacklisted = true
+		end
+	end
 	if canExecute then
-		local input = ReadInt(0x29F89B0-offset)
-		local LoadingIndicator = 0x8E9DA0-offset2
-		local loadmenu = 0x741320-0x56454E
+		local input = ReadInt(inputAddr)
 		if input & 8192 == 8192 and ReadByte(loadmenu) == 0x03 then
 			WriteFloat(LoadingIndicator, 90)
 		end
@@ -51,14 +78,14 @@ end
 			if f ~= nil then
 				WriteString(save1, f:read("*a"))
 				f:close()
-				ConsolePrint("Loaded autosave")
+				print("Loaded autosave")
 			end
 		end
-		if ReadInt(continue+0xC) ~= prevContinue and ReadByte(0x711438-offset2) == 0 and blacklisted == false then
+		if ReadInt(continue+0xC) ~= prevContinue and ReadByte(writeLogic) == 0 and blacklisted == false then
 			local f = io.open("KH2autosave.dat", "wb")
 			f:write(ReadString(continue, 0x10FC0))
 			f:close()
-			ConsolePrint("Wrote autosave")
+			print("Wrote autosave")
 		end
 		prevInput = input
 		prevSave = ReadInt(save1+0xC)
