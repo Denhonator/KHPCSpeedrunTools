ca81423622b5e8edf902569d9ec1575999134f68
codename-geek <155425660+codename-geek@users.noreply.github.com>
2024-07-03 05:51:04 -0700

    Update 2fmSoftReset.lua (#70)

--- 2FMMods/scripts/2fmSoftReset.lua
+++ 2FMMods/scripts/2fmSoftReset.lua
@@ -1,14 +1,16 @@
-local canExecute = false
-
 function _OnInit()
+	canExecute = false
+end
+
+function CheckVersion()
 	if GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then
-		canExecute = true
 		if ReadString(0x09A92F0,4) == 'KH2J' then --EGS
 			ConsolePrint('Epic Version Detected')
 			input = 0x29FAD30
 			softreset = 0xABA69A
 			rng = 0x753350
 			spawns = 0x2AE57B8
+			canExecute = true
 		elseif ReadString(0x09A9830,4) == 'KH2J' or  ReadString(0x09A8830,4) == 'KH2J' then --Steam
 			if ReadString(0x09A9830,4) == 'KH2J' then -- Global
 				ConsolePrint('Steam Global Version Detected')
@@ -21,11 +23,16 @@ function _OnInit()
 			softreset = 0xABABDA - offset
 			rng = 0x7535C0 - offset
 			spawns = 0x2AE5CF8 - offset
+			canExecute = true
 		end
 	end
 end
 
 function _OnFrame()
+	if not canExecute then
+		CheckVersion()
+	end
+
 	if canExecute and ReadInt(input) == 247042 then
 		WriteByte(softreset, 1)
 		WriteInt(rng, 0x00000001)
