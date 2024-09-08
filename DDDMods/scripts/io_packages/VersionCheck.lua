5ddee7a21a2ea0994f2742c828baf3529cbcc020
Chris Hudson <c.ahudson84@yahoo.com>
2024-09-07 18:20:32 -0700

    Initial Epic update.

---
+++ DDDMods/scripts/io_packages/VersionCheck.lua
@@ -0,0 +1,25 @@
+-- Current Versions are 1.0.0.2 (Steam) and 1.0.0.10 Epic Games Store
+EGSGlobalVersion = 0x8A90A6
+-- EGSJPVersion = 0x8A90A6
+SteamGlobalVersion = 0x0
+SteamJPVersion = 0x0
+
+function importVars(file)
+	local slashIdx = string.find(file, "/")
+	if not pcall(require, file) then
+		local errorString = "\n\n!!!!!!!! IMPORT ERROR !!!!!!!!\n\n"
+		local msg = ""
+		if slashIdx then
+			msg = string.format("%s.lua missing, get it from the Github!", string.sub(file, slashIdx + 1, #file))
+		else
+			msg = string.format("%s.lua missing, get it from the Github!", file)
+		end
+		ConsolePrint(string.format("%s%s%s", errorString, msg, errorString))
+	else
+		if slashIdx then
+			ConsolePrint(string.format("Running with %s!", string.sub(file, slashIdx + 1, #file)))
+		else
+			ConsolePrint(string.format("Running with %s!", file))
+		end
+	end
+end
