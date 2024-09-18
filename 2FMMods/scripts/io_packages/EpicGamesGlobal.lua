1cc822eb5cc8a4c03fbc5ea454fd7554c0a82a57
Chris Hudson <c.ahudson84@yahoo.com>
2024-09-18 15:35:50 -0700

    House keeping.

--- 2FMMods/scripts/io_packages/EpicGamesGlobal.lua
+++ 2FMMods/scripts/io_packages/EpicGamesGlobal.lua
@@ -1,37 +1,37 @@
 -- Current Versions are 1.0.0.2 (Steam) and 1.0.0.10 Epic Games Store
 -- shared
+btlTyp = 0x2A10E84
 inputAddress = 0x29FAD70
+location  = 0x716DF8
+save = 0x9A9330
 saveMenuSlot = 0xBED910
 soraHP = 0x2A23018
-location  = 0x716DF8
-btlTyp = 0x2A10E84
-save = 0x9A9330
 
 -- gummi skip
 gummiSkip = 0x9AD327
 
 -- auto save
-continue = 0x29FB000
-saveSelect = 0x2616DA0
 autoSaveAddress = 0x9BA3B0
-loadingIndicator = 0x8EC050
+continue = 0x29FB000
 loadMenu = 0x743350
+loadingIndicator = 0x8EC050
+saveSelect = 0x2616DA0
 writeLogic = 0x713438
 
 -- save anywhere
-valorAddress = 0x2A62594
-saveAddress = 0x2A5CC24
 abilities = 0x2A231E8
+saveAddress = 0x2A5CC24
+valorAddress = 0x2A62594
 
 -- soft reset
-softReset = 0xABA6DA
 rng = 0x753350
+softReset = 0xABA6DA
 spawns = 0x2AE57F8
 
 -- load save menu
-menu = 0x2A10FE0
-loading = 0x8EC053
 cutscene = 0x7281C0
+loading = 0x8EC053
+menu = 0x2A10FE0
 
 -- laser dome skip
 xemnasHP = 0x2A22B28
@@ -40,6 +40,6 @@ xemnasHP = 0x2A22B28
 pause = 0xABB2F8
 
 -- auto attack
+autoAttack = 0x2A5C416
 commandMenuItem = 0x2A1013C
 inDialogue = 0x2A15D90
-autoAttack = 0x2A5C416
