0094a129014606f34fb282da9a429db5653833d6
cahudson94 <c.hudson13@yahoo.com>
2024-10-31 23:52:51 -0700

    Starting conversion to multi version support.

--- 2FMMods/scripts/2fmAutoAttack.lua
+++ 2FMMods/scripts/2fmAutoAttack.lua
@@ -2,24 +2,9 @@ LUAGUI_NAME = "2fmAutoSave"
 LUAGUI_AUTH = "denhonator (edited by deathofall84)"
 LUAGUI_DESC = "Hold attack to combo"
 
-local canExecute = false
-
 function _OnInit()
-	if GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then --PC
-        canExecute = true
+	if GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then
 		require("VersionCheck")
-		if ReadByte(EGSGlobalVersion) == 106 then
-			importVars("EpicGamesGlobal")
-		elseif ReadByte(EGSJPVersion) == 106 then
-			importVars("EpicGamesJP")
-		elseif ReadByte(SteamGlobalVersion) == 106 then
-			importVars("SteamGlobal")
-		elseif ReadByte(SteamJPVersion) == 106 then
-			importVars("SteamJP")
-		else
-			canExecute = false
-			ConsolePrint("\n\n!!!!!!!! VERSION ERROR !!!!!!!!\n\nVersion check failed, check variable file version numbers against game version")
-		end
 	end
 end
 
---
+++ 2FMMods/scripts/io_packages/SteamJP_1_0_0_2.lua
@@ -0,0 +1,45 @@
+-- Current Versions are 1.0.0.2 (Steam) and 1.0.0.10 Epic Games Store
+-- shared
+btlTyp = 0x2A11404
+inputAddress = 0xBF31A0
+location  = 0x717008
+save = 0x9A98B0
+saveMenuSlot = 0xBEDE90
+soraHP = 0x2A23598
+
+-- gummi skip
+gummiSkip = 0x9AD8A7
+
+-- auto save
+autoSaveAddress = 0x9BA930
+continue = 0x29FB580
+loadingIndicator = 0x8EC5B0
+loadMenu = 0x7435D0
+saveSelect = 0x26174E0
+writeLogic = 0x7134A0
+
+-- save anywhere
+abilities = 0x2A23768
+saveAddress = 0x2A5D1A4
+valorAddress = 0x2A62B14
+
+-- soft reset
+rng = 0x7535C0
+softReset = 0xABAC5A
+spawns = 0x2AE5D78
+
+-- load save menu
+cutscene = 0x728440
+loading = 0x8EC5B3
+menu = 0x2A11560
+
+-- laser dome skip
+xemnasHP = 0x2A230A8
+
+-- credit skip
+pause = 0xABB878
+
+-- auto attack
+autoAttack = 0x2A5C996
+commandMenuItem = 0x2A106BC
+inDialogue = 0x2A16310
